5c6605262ba0afa21ae96ea5c5e4785f7a33d931
Lionel Montesdeoca <lionelmontesdeocanuez.com>
2021-01-25 14:12:08 +0000

    index.js and enviroment

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoRoute = require('./enviroment');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+mongoose.connect(`${mongoRoute}`)
+
+app.use(cors({ origin : true}  ));
+app.use(express.json())
+
+/* const itemsRouter = require('./api/items/items.router')
+app.use('/items', itemsRouter) */
+
+app.listen(5000)
